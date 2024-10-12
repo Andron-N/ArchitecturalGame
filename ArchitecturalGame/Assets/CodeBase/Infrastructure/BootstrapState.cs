440d42f978d1e9ab4723641a44f359002c895c48
NaumenkoAndrei <an6303650@gmail.com>
2024-10-12 20:01:41 +0400

    Adding and realise Initial Scene

--- ArchitecturalGame/Assets/CodeBase/Infrastructure/BootstrapState.cs
+++ ArchitecturalGame/Assets/CodeBase/Infrastructure/BootstrapState.cs
@@ -5,16 +5,26 @@ namespace CodeBase.Infrastructure
 {
 	public class BootstrapState : IState
 	{
-		private readonly GameStateMachine _stateMachine;
+		private const string Initial = "Initial";
 
-		public BootstrapState(GameStateMachine stateMachine)
+		private readonly GameStateMachine _stateMachine;
+		private readonly SceneLoader _sceneLoader;
+
+		public BootstrapState(GameStateMachine stateMachine, SceneLoader sceneLoader)
 		{
 			_stateMachine = stateMachine;
+			_sceneLoader = sceneLoader;
 		}
 
 		public void Enter()
 		{
 			RegisterServices();
+			_sceneLoader.Load(Initial, onLoaded: EnterLoadLevel);
+		}
+
+		private void EnterLoadLevel()
+		{
+			_stateMachine.Enter<LoadLevelState, string>("Main");
 		}
 
 		public void Exit()
