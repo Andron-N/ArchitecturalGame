50b06d037c4b2a96bfd4daecb98040cc0f48d05d
NaumenkoAndrei <an6303650@gmail.com>
2024-10-22 21:17:22 +0400

    Dependency Injection : Refactoring

--- ArchitecturalGame/Assets/CodeBase/Infrastructure/Fabric/GameFactory.cs
+++ ArchitecturalGame/Assets/CodeBase/Infrastructure/Fabric/GameFactory.cs
@@ -1,7 +1,7 @@
 ﻿using CodeBase.Infrastructure.AssetManagement;
 using UnityEngine;
 
-namespace CodeBase.Infrastructure
+namespace CodeBase.Infrastructure.Fabric
 {
 	public class GameFactory : IGameFactory
 	{
