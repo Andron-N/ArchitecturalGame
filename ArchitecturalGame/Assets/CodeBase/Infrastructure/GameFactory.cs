c2823bf41ad1e574ce22cc9c74f3bea30260e7f3
NaumenkoAndrei <an6303650@gmail.com>
2024-10-22 20:07:38 +0400

    Dependency Injection : Implemended Asset Provider

--- ArchitecturalGame/Assets/CodeBase/Infrastructure/GameFactory.cs
+++ ArchitecturalGame/Assets/CodeBase/Infrastructure/GameFactory.cs
@@ -1,28 +1,21 @@
-﻿using UnityEngine;
+﻿using CodeBase.Infrastructure.AssetManagement;
+using UnityEngine;
 
 namespace CodeBase.Infrastructure
 {
 	public class GameFactory : IGameFactory
 	{
-		private const string HeroPath = "Hero/hero";
-		private const string HudPath = "Hud/Hud";
+		private readonly IAssetProvider _asset;
+
+		public GameFactory(IAssetProvider asset)
+		{
+			_asset = asset;
+		}
 
 		public GameObject CreateHero(GameObject at) =>
-			Instantiate(HeroPath, at: at.transform.position);
+			_asset.Instantiate(AssetPath.HeroPath, at: at.transform.position);
 
 		public void CreateHud() =>
-			Instantiate(HudPath);
-
-		private static GameObject Instantiate(string path, Vector3 at)
-		{
-			GameObject prefab = Resources.Load<GameObject>(path);
-			return Object.Instantiate(prefab, at, Quaternion.identity);
-		}
-
-		private static GameObject Instantiate(string path)
-		{
-			GameObject prefab = Resources.Load<GameObject>(path);
-			return Object.Instantiate(prefab);
-		}
+			_asset.Instantiate(AssetPath.HudPath);
 	}
 }
