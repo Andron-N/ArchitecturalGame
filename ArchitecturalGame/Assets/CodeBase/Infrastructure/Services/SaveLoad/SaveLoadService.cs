35aa443cc7509b462b051ab4027d3fea12a33a26
Andron-N <andron-nwork@gmail.com>
2024-11-03 16:15:19 +0400

    Enemies : Adding Aggro

--- ArchitecturalGame/Assets/CodeBase/Infrastructure/Services/SaveLoad/SaveLoadService.cs
+++ ArchitecturalGame/Assets/CodeBase/Infrastructure/Services/SaveLoad/SaveLoadService.cs
@@ -1,5 +1,5 @@
 ﻿using CodeBase.Data;
-using CodeBase.Infrastructure.Fabric;
+using CodeBase.Infrastructure.Factory;
 using CodeBase.Infrastructure.Services.PersistentProgress;
 using UnityEngine;
 
