7c8841d4635539acbbfd28f2eb02454f115ce9bf
Andron-N <andron-nwork@gmail.com>
2024-11-08 20:01:17 +0400

    Refactoring

--- ArchitecturalGame/Assets/CodeBase/Enemy/AgentMoveToPlayer.cs
+++ ArchitecturalGame/Assets/CodeBase/Enemy/AgentMoveToPlayer.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Factory;
 using CodeBase.Infrastructure.Services;
 using UnityEngine;
 using UnityEngine.AI;
@@ -44,52 +43,3 @@ namespace CodeBase.Enemy
 			Vector3.Distance(_agent.transform.position, _heroTransform.position) >= MinimalDistance;
 	}
 }
-||||||| 087d73c
-=======
-﻿using CodeBase.Infrastructure.Factory;
-using CodeBase.Infrastructure.Services;
-using UnityEngine;
-using UnityEngine.AI;
-
-namespace CodeBase.Enemy
-{
-	public class AgentMoveToPlayer : MonoBehaviour
-	{
-		private const float MinimalDistance = 1;
-
-		[SerializeField] private NavMeshAgent _agent;
-
-		private Transform _heroTransform;
-		private IGameFactory _gameFactory;
-
-		private void Start()
-		{
-			_gameFactory = AllServices.Container.Single<IGameFactory>();
-
-			if(_gameFactory.HeroGameObject != null)
-				InstantiateHeroTransform();
-			else
-				_gameFactory.HeroCreated += HeroCreated;
-		}
-
-		private void Update()
-		{
-			if(Initialized() && HeroNotReached())
-				_agent.destination = _heroTransform.position;
-
-		}
-
-		private void InstantiateHeroTransform() =>
-			_heroTransform = _gameFactory.HeroGameObject.transform;
-
-		private void HeroCreated() =>
-			InstantiateHeroTransform();
-
-		private bool Initialized() =>
-			_heroTransform != null;
-
-		private bool HeroNotReached() =>
-			Vector3.Distance(_agent.transform.position, _heroTransform.position) >= MinimalDistance;
-	}
-}
->>>>>>> origin/Enemies
